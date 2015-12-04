225d0de16f5066495568ed2329b6e4d8a396566f
vk <crazyvk56@gmail.com>
2015-12-04 18:38:16 +0530

    test

--- AppDelegate.m
+++ AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    [GMSServices provideAPIKey:@"AIzaSyCZmpkXLhnZggmTPEQxiC0zwwrQGJR5XJc"];
+    [GMSServices provideAPIKey:@"You google map key"];
     
     return YES;
 }
